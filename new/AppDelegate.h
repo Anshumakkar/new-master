71a61400256357373c42b4c5e7281434898a81e6
Harkiranjeet <subodh@Harkiran.local>
2012-12-01 15:56:29 +0530

    sdbf
    esdrfg

---
+++ new/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  new
+//
+//  Created by Anshu on 12/1/12.
+//  Copyright (c) 2012 Anshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
